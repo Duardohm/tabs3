2be443f9ecadee69b4dba5ccb26b2a904b162122
Eduardo <edu.pereira3355@gmail.com>
2023-06-12 20:56:50 -0300

    atividade

--- Ionic-projects/lista3/src/app/services/senhas.service.ts
+++ Ionic-projects/lista3/src/app/services/senhas.service.ts
@@ -5,5 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class SenhasService {
 
+  public senhasGeral: number = 0;
+  public senhasPrior: number = 0;
+  public senhasExame: number = 0;
+  public senhasTotal: number = 0;
+  
   constructor() { }
+
+  somaGeral() {this.senhasGeral++;this.senhasTotal++;}
+  somaPrior() {this.senhasPrior++;this.senhasTotal++;}
+  somaExame() {this.senhasExame++;this.senhasTotal++;}
+
+  
 }
